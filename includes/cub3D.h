1c8262682e08ac92959a1fd5c8d6ef3b1d18352d
wmari <wmari@student.42.fr>
2023-06-20 14:02:44 +0200

    working slowly

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: wmari <wmari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:28:37 by wmari             #+#    #+#             */
-/*   Updated: 2023/06/20 12:35:50 by wmari            ###   ########.fr       */
+/*   Updated: 2023/06/20 14:00:47 by wmari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,25 @@
 # include "minilibx-linux/mlx_int.h"
 # include <math.h>
 
+#define NORTH	1
+#define SOUTH	2
+#define EAST	3
+#define WEST	4
+
+typedef struct	s_sprite
+{
+	int		direction;
+	char	*path_to_file;
+}				t_sprite;
+
+typedef struct	s_world
+{
+	void		*mlx;
+	void		*mlx_win;
+	t_sprite	northWall;
+	t_sprite	southWall;
+	t_sprite	eastWall;
+	t_sprite	westWall;
+	
+}				t_world;
 #endif
