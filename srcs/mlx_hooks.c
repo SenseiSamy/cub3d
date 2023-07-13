65b1354b5508aa5fc10ce3a4ccdf12b12e2bbd73
wmari <wmari@student.42.fr>
2023-07-13 16:38:07 +0200

    cleaning

--- srcs/mlx_hooks.c
+++ srcs/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: wmari <wmari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:39:48 by snaji             #+#    #+#             */
-/*   Updated: 2023/07/13 15:06:53 by wmari            ###   ########.fr       */
+/*   Updated: 2023/07/13 16:37:53 by wmari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,5 +108,6 @@ void	set_hooks(t_world *world)
 {
 	mlx_hook(world->mlx_win, 2, 1L << 0, keypresses, world);
 	mlx_hook(world->mlx_win, 3, 1L << 1, keyrelease, world);
+	
 	mlx_hook(world->mlx_win, 17, 0L, exit_cub3d, world);
 }
