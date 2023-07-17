d8b517c438f6f299e11ca255bc05587fd45a0f63
wmari <wmari@student.42.fr>
2023-07-17 13:42:24 +0200

    working on collision

--- srcs/mlx_hook_utils.c
+++ srcs/mlx_hook_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_hook_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
+/*   By: wmari <wmari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 13:51:36 by wmari             #+#    #+#             */
-/*   Updated: 2023/07/16 20:03:51 by snaji            ###   ########.fr       */
+/*   Updated: 2023/07/17 13:29:24 by wmari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,30 +66,65 @@ int	keys_is_not_pressed(t_world *world)
 	return (1);
 }
 
+int	too_close_of_wall(t_world *world, int dir)
+{
+	if (dir == UP)
+	{
+		if (world->map[(int)(world->pos.y + (0.03 * world->dir.y) + 1.5)][(int)(world->pos.x + (0.03 * world->dir.x) + 1.5)] != '0')
+			return (0);
+		return (printf("map[%lf][%lf] = %c\nplayer pos[%lf][%lf]\n",(world->pos.y + (0.03 * world->dir.y) + 1.5), (world->pos.x + (0.03 * world->dir.x) + 1.5), world->map[(int)(world->pos.y + (0.03 * world->dir.y) + 1.5)][(int)(world->pos.x + (0.03 * world->dir.x) + 1.5)], world->pos.y, world->pos.x), 1);
+	}
+	if (dir == DOWN)
+	{
+		if (world->map[(int)(world->pos.y - (0.03 * world->dir.y) - 1.5)][(int)(world->pos.x - (0.03 * world->dir.x) - 1.5)] != '0')
+			return (0);
+		return (printf("map[%lf][%lf] = %c\nplayer pos[%lf][%lf]\n",(world->pos.y + (0.03 * world->dir.y) + 1.5), (world->pos.x + (0.03 * world->dir.x) + 1.5), world->map[(int)(world->pos.y + (0.03 * world->dir.y) + 1.5)][(int)(world->pos.x + (0.03 * world->dir.x) + 1.5)], world->pos.y, world->pos.x), 1);
+
+	}
+	if (dir == RIGHT)
+	{
+		if (world->map[(int)(world->pos.y + (0.03 * world->plane.y) + 1.5)][(int)(world->pos.x + (0.03 * world->plane.x) + 1.5)] != '0')
+			return (0);
+		return (printf("map[%lf][%lf] = %c\nplayer pos[%lf][%lf]\n",(world->pos.y + (0.03 * world->dir.y) + 1.5), (world->pos.x + (0.03 * world->dir.x) + 1.5), world->map[(int)(world->pos.y + (0.03 * world->dir.y) + 1.5)][(int)(world->pos.x + (0.03 * world->dir.x) + 1.5)], world->pos.y, world->pos.x), 1);
+	}
+	if (dir == LEFT)
+	{
+		if (world->map[(int)(world->pos.y - (0.03 * world->plane.y) - 1.5)][(int)(world->pos.x - (0.03 * world->plane.x) - 1.5)] != '0')
+			return (0);
+		return (printf("map[%lf][%lf] = %c\nplayer pos[%lf][%lf]\n",(world->pos.y + (0.03 * world->dir.y) + 1.5), (world->pos.x + (0.03 * world->dir.x) + 1.5), world->map[(int)(world->pos.y + (0.03 * world->dir.y) + 1.5)][(int)(world->pos.x + (0.03 * world->dir.x) + 0.3)], world->pos.y, world->pos.x), 1);
+	}
+	return (0);
+}
+
 int	can_move_in_dir(t_world *world, int dir)
 {
 	int	new_x;
 	int	new_y;
 
-	if (dir == UP)
+	if (dir == UP && too_close_of_wall(world, dir))
 	{
 		new_x = (int)(world->pos.x + (0.03 * world->dir.x));
 		new_y = (int)(world->pos.y + (0.03 * world->dir.y));
 	}
-	else if (dir == DOWN)
+	else if (dir == DOWN && too_close_of_wall(world, dir))
 	{
 		new_x = (int)(world->pos.x - (0.03 * world->dir.x));
 		new_y = (int)(world->pos.y - (0.03 * world->dir.y));
 	}
-	else if (dir == RIGHT)
+	else if (dir == RIGHT && too_close_of_wall(world, dir))
 	{
 		new_x = (int)(world->pos.x - (0.03 * world->plane.x));
 		new_y = (int)(world->pos.y - (0.03 * world->plane.y));
 	}
-	else
+	else if (dir == LEFT && too_close_of_wall(world, dir))
 	{
 		new_x = (int)(world->pos.x + (0.03 * world->plane.x));
 		new_y = (int)(world->pos.y + (0.03 * world->plane.y));
+	}
+	else
+	{
+		new_x = (int)(world->pos.x);
+		new_y = (int)(world->pos.y);
 	}
 	if (world->map[new_y] && (world->map[new_y][new_x] == '1'
 		|| world->map[new_y][new_x] == '2'))
